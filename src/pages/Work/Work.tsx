6836584e88772bf139e21d3778f28c1912f94f31
Falujo Adeyemi <falujoadeyemi6776@gmail.com>
2025-06-16 12:24:23 +0100

    ...fix lazy iamge loading

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,9 +1,25 @@
-import "./Work.css"
+import "./Work.css";
+// images
+// import Logo1 from "../../assets/images/Logo 1.svg";
+// import Logo2 from "../../assets/images/Logo 2.svg";
+// import Logo3 from "../../assets/images/Logo 3.svg";
+// import Logo4 from "../../assets/images/Logo 4.svg";
+// import Logo5 from "../../assets/images/Logo 5.svg";
+// import Logo6 from "../../assets/images/Logo 6.svg";
+// import Logo7 from "../../assets/images/Logo 7.svg";
+// import Logo8 from "../../assets/images/Logo 8.svg";
+
+// import profile1 from "../../assets/images/Profile 1.svg";
+// import profile2 from "../../assets/images/Profile 2.svg";
+// import profile3 from "../../assets/images/Profile 3.svg";
+// import profile4 from "../../assets/images/Profile 4.svg";
+// import profile5 from "../../assets/images/Profile 5.svg";
+// import profile6 from "../../assets/images/Profile 6.svg";
 
 export default function Work() {
-    return(
-        <>
-        <h1>work</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>work</h1>
+    </>
+  );
 }
