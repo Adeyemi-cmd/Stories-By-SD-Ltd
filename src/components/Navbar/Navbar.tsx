e47b1a969f939dd0f1958d908d96946743117e33
Falujo Adeyemi <falujoadeyemi6776@gmail.com>
2025-06-21 12:39:21 +0100

    feat: implement functioning  menu-icon toggle for mobile nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,14 +3,17 @@ import "./Navbar.css";
 import Logo from "../../assets/logo.svg";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOPen] = useState(false);
   return (
     <>
       <header className="header">
         <div className="container">
           <nav>
-            <ul role="list" className="Navlist">
+            <ul role="list" className={`Navlist ${isMenuOpen ? "active" : ""}`}>
+              {" "}
               <li>
                 <Link to={"/"} className="Link">
                   Services
@@ -26,6 +29,13 @@ export default function Navbar() {
                   About
                 </Link>
               </li>
+              <li className="mobile_menu">
+                <Link to="/contact">
+                  <button className=" menu_btn">
+                    <span>Get in touch</span>
+                  </button>
+                </Link>
+              </li>
             </ul>
           </nav>
 
@@ -39,7 +49,7 @@ export default function Navbar() {
             </button>
           </Link>
 
-          <div className="menu">
+          <div className="menu" onClick={() => setIsMenuOPen(!isMenuOpen)}>
             <i className="bx bx-menu"></i>
           </div>
         </div>
